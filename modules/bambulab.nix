1ac472b54eafa82c3270630dcae45c08535b22ab
micartey <me@micartey.dev>
2024-11-24 22:08:20 +0100

    update bambulab

--- modules/bambulab.nix
+++ modules/bambulab.nix
@@ -1,34 +1,36 @@
 { config, pkgs, ... }:
 
-{
-    bambu-studio-appimage = pkgs.appimageTools.wrapType2 rec {
-        name = "BambuStudio";
-        pname = "bambustudio";
-        version = "01.09.00.60";
+let
+bambulab = pkgs.appimageTools.wrapType2 rec {
+    name = "BambuStudio";
+    pname = "bambustudio";
+    version = "01.10.01.50";
 
-        src = pkgs.fetchurl {
-            url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_linux_ubuntu_${version}.AppImage";
-            sha256 = "sha256-qxV6pn1OVU/IGS2Lv954gm9ud1MtBr8Khdy2tJvkwj8=";
-        };
-
-        profile = ''
-        export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-        export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
-        '';
-
-        extraPkgs = pkgs: with pkgs; [
-            cacert
-            curl
-            glib
-            glib-networking
-            gst_all_1.gst-plugins-bad
-            gst_all_1.gst-plugins-base
-            gst_all_1.gst-plugins-good
-            webkitgtk
-        ];
+    src = pkgs.fetchurl {
+        url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_linux_ubuntu_24.04_v${version}.AppImage ";
+        sha256 = "sha256-NH7SwQzg5Dmbn+5E2ov7EuVp4+kxgTebBRBM71ntxD8=";
     };
 
+    profile = ''
+    export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
+    '';
+
+    extraPkgs = pkgs: with pkgs; [
+        cacert
+        curl
+        glib
+        glib-networking
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        webkitgtk_4_1
+    ];
+};
+in
+
+{
     environment.systemPackages = with pkgs; [
-        bambu-studio-appimage
+        bambulab
     ];
 }
