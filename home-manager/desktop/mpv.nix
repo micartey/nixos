3cc576c4a21305c5dd858abc9df3e1de6d78c27e
micartey <me@micartey.dev>
2025-02-14 17:10:48 +0100

    package mpv shaders in zip

--- home-manager/desktop/mpv.nix
+++ home-manager/desktop/mpv.nix
@@ -1,12 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   catppuccin.mpv.enable = false; # This needs to be disabled for mpvconf to be written
   programs.mpv.enable = true;
 
   home.file = {
-    shaders = { source = ../../dots/mpv/shaders; target = ".config/mpv/shaders"; };
-    inputconf = { source = ../../dots/mpv/input.conf; target = ".config/mpv/input.conf"; };
-    mpvconf = { source = ../../dots/mpv/mpv.conf; target = ".config/mpv/mpv.conf"; };
+    shaders = {
+      source =
+        pkgs.runCommand "mpv-shaders"
+          {
+            buildInputs = [ pkgs.unzip ];
+            shadersZip = ../../dots/mpv/shaders.zip;
+          }
+          ''
+            unzip -j "$shadersZip" -d "$out"
+          '';
+      target = ".config/mpv/shaders";
+    };
+
+    inputconf = {
+      source = ../../dots/mpv/input.conf;
+      target = ".config/mpv/input.conf";
+    };
+
+    mpvconf = {
+      source = ../../dots/mpv/mpv.conf;
+      target = ".config/mpv/mpv.conf";
+    };
   };
 }
