d72c4c2b76a49650f4308c68f286bde380b3d510
micartey <me@micartey.dev>
2025-04-29 21:53:24 +0200

    airplay capabilities

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -1,10 +1,26 @@
 { pkgs, ... }:
 
 {
+  networking.firewall.allowedUDPPorts = [
+    6001
+    6002
+  ];
+
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+
+    raopOpenFirewall = true;
+    extraConfig.pipewire = {
+      "10-airplay" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+          }
+        ];
+      };
+    };
 
     alsa.enable = true;
     alsa.support32Bit = true;
