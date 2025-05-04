858350e642e93b2fa9e8e306c7e97aea3b1ed463
micartey <me@micartey.dev>
2025-05-05 00:32:51 +0200

    davinci resolve

---
+++ home-manager/desktop/davinci-resolve.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.davinci-resolve
+  ];
+
+  xdg.desktopEntries = {
+    "davinci-custom" = {
+      name = "Davinci Resolve (Fix)";
+      genericName = "Video Editor";
+      comment = "Davinci Resolve with a launch fixes";
+      exec = "env ROC_ENABLE_PRE_VEGA=1 env RUSTICL_ENABLE=amdgpu,amdgpu-pro,radv,radeon env DRI_PRIME=1 env QT_QPA_PLATFORM=xcb davinci-resolve";
+      icon = "davinci-resolve";
+    };
+  };
+}
