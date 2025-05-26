e1919d0a288bd4299d8c6d755cca3fc4e04047b7
micartey <me@micartey.dev>
2025-05-27 00:25:56 +0200

    remove moonlight

--- modules/applications/sunshine.nix
+++ modules/applications/sunshine.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  environment.systemPackages = with pkgs; [ moonlight-qt ];
+  # I don't need moonlight here on my main desktop as this is often the host
+  # environment.systemPackages = with pkgs; [ moonlight-qt ];
 
   services.sunshine = {
     enable = true;
