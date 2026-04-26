43852d9b49351da310c9816d5c06a6629c2c6a0c
micartey <me@micartey.dev>
2026-04-26 16:46:20 +0200

    librepods add capabilities

--- modules/applications/librepods.nix
+++ modules/applications/librepods.nix
@@ -4,4 +4,11 @@
   environment.systemPackages = [
     inputs.librepods.packages.x86_64-linux.default
   ];
+
+  security.wrappers.librepods = {
+    source = "${inputs.librepods.packages.x86_64-linux.default}/bin/librepods";
+    capabilities = "cap_net_admin+eip";
+    owner = "root";
+    group = "root";
+  };
 }
