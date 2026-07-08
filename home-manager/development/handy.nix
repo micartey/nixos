180a50c35ada22e1267406a87e3a98574039faad
micartey <me@micartey.dev>
2026-07-08 22:24:01 +0200

    improve handy keybinds

--- home-manager/development/handy.nix
+++ home-manager/development/handy.nix
@@ -1,7 +1,26 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
 
+let
+  mkLua = lib.generators.mkLuaInline;
+in
 {
   home.packages = [
     inputs.handy.packages.${pkgs.system}.default
+
+    pkgs.wtype
+  ];
+
+  wayland.windowManager.hyprland.settings.bind = [
+    {
+      _args = [
+        "SUPER + C"
+        (mkLua "hl.dsp.exec_cmd(\"sleep 0.3; pkill -USR2 -x handy || pkill -USR2 -x .handy-wrapped\")")
+      ];
+    }
   ];
 }
